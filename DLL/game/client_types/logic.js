2723a38096a3a60c0a3dd029840b3eb5a52d55e0
Slendercoder <33577755+Slendercoder@users.noreply.github.com>
2017-11-20 13:23:51 -0500

    Add files via upload

---
+++ DLL/game/client_types/logic.js
@@ -0,0 +1,91 @@
+/**
+ * # Logic type implementation of the game stages
+ * Copyright(c) 2017 Edgar Andrade-Lotero <edgar.andrade@urosario.edu.co>
+ * MIT Licensed
+ *
+ * http://www.nodegame.org
+ * ---
+ */
+
+"use strict";
+
+var ngc = require('nodegame-client');
+var stepRules = ngc.stepRules;
+var constants = ngc.constants;
+var J = ngc.JSUS;
+var counter = 0;
+
+module.exports = function(treatmentName, settings, stager, setup, gameRoom) {
+
+    var node = gameRoom.node;
+    var channel =  gameRoom.channel;
+
+    // Must implement the stages here.
+
+    // Increment counter.
+    counter = counter ? ++counter : settings.SESSION_ID || 1;
+
+    stager.setOnInit(function() {
+
+        // Initialize the client.
+    });
+
+    stager.extendStep('instructions', {
+        cb: function() {
+            console.log('Instructions.');
+        }
+    });
+
+    stager.extendStep('game', {
+        // matcher: {
+        //     roles: [ 'PLAYER_A', 'PLAYER_B' ],
+        //     match: 'round_robin',
+        //     cycle: 'mirror_invert',
+        //     sayPartner: false
+        //     // skipBye: false,
+        //
+        // },
+        cb: function() {
+            console.log('\n%%%%%%%%%%%%%%%');
+            console.log('Game round: ' + node.player.stage.round);
+            doMatch();
+        }
+    });
+
+    stager.extendStep('end', {
+        cb: function() {
+            node.game.memory.save(channel.getGameDir() + 'data/data_' +
+                                  node.nodename + '.json');
+        }
+    });
+
+    stager.setOnGameOver(function() {
+
+        // Something to do.
+
+    });
+
+    // Here we group together the definition of the game logic.
+    return {
+        nodename: 'lgc' + counter,
+        // Extracts, and compacts the game plot that we defined above.
+        plot: stager.getState(),
+
+    };
+
+    // Helper functions.
+
+    function doMatch() {
+        var players, len;
+        // len = node.game.pl.size();
+        // players = node.game.pl.shuffle().id.getAllKeys();
+
+        players = node.game.pl.id.getAllKeys();
+
+        node.say('Settings', players[0],
+                 [players[1], 2, 8]);
+        node.say('Settings', players[1],
+                 [players[0], 8, 2]);
+    }
+
+};
