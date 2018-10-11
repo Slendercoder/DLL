ce5f7716dce1284c3d337f9528e12175b955228e
AlejandroVelascoG <javier.velasco8@gmail.com>
2018-10-11 12:45:45 -0500

    popup más grande

--- levels/interaccionParejas/game/client_types/player.js
+++ levels/interaccionParejas/game/client_types/player.js
@@ -402,7 +402,7 @@
           node.on.data('Comunicacion', function(msg) {
             // AQUI POPUP DE LE ENVIARON UN MENSAJE
             node.emit('Muestra_Popup');
-            W.setInnerHTML('notif', "¡TIENE UN MENSAJE NUEVO!");
+            W.setInnerHTML('notif', "<br> ¡TIENE UN MENSAJE NUEVO!");
 
             // Agrega el mensaje a la lista
             var opt = document.createElement('option'); // Crea un item nuevo para la lista desplegable
